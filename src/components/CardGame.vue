4cbce31728f3e2b6dccabd777d143a6b87f3c22f
MaionePietro <pietromaionespam@gmail.com>
2023-03-06 14:56:42 +0100

    FIX card

--- src/components/CardGame.vue
+++ src/components/CardGame.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <div v-for="n in 20" @click="debug()">
-        <div class="card" style="width: 12rem;">
-            <img :src="deskOfCards.characters.data[n].card_images" class="card-img-top" alt="...">
+        <div class="card text-center" style="width: 12rem;">
+            <img :src="deskOfCards.characters.data[n].card_images.image_url" class="card-img-top" alt="...">
         <div class="card-body">
         <p class="card-text">{{deskOfCards.characters.data[n].name}}</p>
         <p class="card-text">{{deskOfCards.characters.data[n].archetype}}</p>
@@ -14,17 +14,12 @@ export default {
     props: {
         deskOfCards: {
             Object,
-            //required: false
+            required: false
         }
     },
     data() {
         return {
 
-        }
-    },
-    methods: {
-        debug() {
-            console.log("test: " + deskOfCards.title )
         }
     }
 }
